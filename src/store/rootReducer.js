f3b504be3bd58667121da6979bd72b5ea6b789d8
Kyrylo Storozhenko <>
2020-03-30 12:01:20 +0300

    Redux-form added

--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -1,8 +1,11 @@
 import {combineReducers} from "redux";
+import { reducer as formReducer } from 'redux-form'
 import starterReducer from "../containers/StarterContainer/starterReducer";
+
 
 const rootReducer = combineReducers({
   starterReducer: starterReducer,
+  form: formReducer,
 });
 
 export default rootReducer;
